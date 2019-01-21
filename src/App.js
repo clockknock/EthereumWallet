9c87738d1cfc4045451f5c8a7d3f966627b36236
cypher.zhong <1992zwm@gmail.com>
2019-01-21 14:29:14 +0800

    update add english description

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ class App extends React.Component {
         let {web3, account, transValue, transferTo, transferMsg} = this.state;
         let hexMsg = Buffer.from(transferMsg, "utf-8").toString("hex");
         console.log(hexMsg);
+        alert("转账开始,等待确认/start transfer, waiting confirm");
         let receipt = await web3.eth.sendTransaction({
             from: account,
             to: transferTo,
@@ -86,7 +87,7 @@ class App extends React.Component {
             gas: '100000',
             data: hexMsg
         });
-        alert("转账成功,交易hash是:" + receipt.transactionHash);
+        alert("转账成功,交易hash是/transfer success, trans hash is:" + receipt.transactionHash);
         //转账后重新获取一下余额
         this.getBalance();
         console.table(receipt);
@@ -175,7 +176,7 @@ class App extends React.Component {
                 <tr key={index}>
                     <td>{trans.hash}</td>
                     <td>{trans.timeStamp}</td>
-                    <td>{new Date(parseInt(trans.timeStamp,10) * 1000).toLocaleString()}</td>
+                    <td>{new Date(parseInt(trans.timeStamp, 10) * 1000).toLocaleString()}</td>
                     <td>{trans.value}</td>
                 </tr>
             )
@@ -197,57 +198,61 @@ class App extends React.Component {
             <div className="App">
                 <br/>
                 <br/>
-                <h1>Rinkeby测试网络钱包</h1>
+                <h1>Rinkeby测试网络钱包/Rinkeby Testnet Wallet</h1>
 
-                <input type="button" value={"点击获取助记词"} onClick={this.generateMnemonic}/>
+                <input type="button" value={"点击获取助记词/Click To Generate Mnemonic"} onClick={this.generateMnemonic}/>
                 <br/>
-                <input type="button" value={"根据助记词生成帐号(同时生成web3对象)"} onClick={this.generatePubAndPrivFromMnemonic}/>
+                <input type="button" value={"根据助记词生成帐号(同时生成web3对象)/generate Pubkey And Privkey From Mnemonic"}
+                       onClick={this.generatePubAndPrivFromMnemonic}/>
                 <br/>
-                <textarea cols="30" rows="10" onChange={this.onMnemonicTextareaChange} placeholder={"有助记词?输入助记词"}
+                <textarea cols="30" rows="10" onChange={this.onMnemonicTextareaChange}
+                          placeholder={"有助记词?输入助记词/input your mnemonic if you have one"}
                           value={this.state.mnemonic}/>
                 <br/>
-                <h2>你的公钥:</h2>
-                <textarea value={this.state.account} placeholder={"你的公钥"} disabled="true"/>
+                <h2>你的公钥/your pubkey:</h2>
+                <textarea value={this.state.account} placeholder={"你的公钥/your pubkey"} disabled="true"/>
                 <br/>
                 <canvas id={"publicKeyQrCode"}/>
                 <hr/>
 
                 <div>
-                    <h2>从私钥恢复account</h2>
-                    <textarea placeholder={"请输入备份好的私钥"} onChange={this.onPrivKeyInputChange}/>
+                    <h2>从私钥恢复account/recover account from privkey</h2>
+                    <textarea placeholder={"请输入备份好的私钥/input your privkey"} onChange={this.onPrivKeyInputChange}/>
                     <br/>
-                    <input type="button" value={"开始恢复"} onClick={this.restoreAccountFromPrivateKey}/>
-                    <p>恢复出来的帐号:</p>
-                    <textarea value={this.state.restoreFromPrivateKey.address} placeholder={"恢复出来的帐号"} disabled="true"/>
+                    <input type="button" value={"开始恢复/start recorver"} onClick={this.restoreAccountFromPrivateKey}/>
+                    <p>恢复出来的帐号:recovered account</p>
+                    <textarea value={this.state.restoreFromPrivateKey.address} placeholder={"恢复出来的帐号/recovered account"}
+                              disabled="true"/>
                 </div>
                 <hr/>
 
                 <div>
-                    <h2>查看余额</h2>
+                    <h2>查看余额/get balance</h2>
                     <p>
-                        <input type="button" value="查看余额" onClick={this.getBalance}/>
+                        <input type="button" value="查看余额/get balance" onClick={this.getBalance}/>
                     </p>
                     <p>{this.state.balance}Wei</p>
                 </div>
                 <hr/>
 
                 <div>
-                    <h2>转账</h2>
+                    <h2>转账/transfer</h2>
                     to:<textarea value={this.state.transferTo} onChange={this.onTransferToChange}/><br/>
                     value:<input type="number" value={this.state.transValue}
-                                 onChange={this.onTransferValueChange}/><br/>
+                                 onChange={this.onTransferValueChange}/>wei<br/>
                     msg:<textarea value={this.state.transferMsg} onChange={this.onTransferMsgChange}/><br/>
-                    <input type="button" value={"执行转账"} onClick={this.transferTo}/>
+                    <input type="button" value={"执行转账/transfer"} onClick={this.transferTo}/>
                 </div>
                 <div>
-                    <h2>获取历史交易</h2>
-                    <input type="button" value={"获取历史交易"} onClick={this.getTransactionInformation}/>
+                    <h2>获取历史交易/get history transactions</h2>
+                    <input type="button" value={"获取历史交易/get history transactions"}
+                           onClick={this.getTransactionInformation}/>
                     <table>
                         <thead>
                         <tr>
                             <td>txHash</td>
                             <td>timeStamp</td>
-                            <td>转换后的</td>
+                            <td>date</td>
                             <td>value</td>
                         </tr>
                         </thead>
@@ -259,12 +264,12 @@ class App extends React.Component {
                 <hr/>
 
                 <div>
-                    <h2>其他相关方法</h2>
+                    <h2>其他相关方法/other method</h2>
                     <hr/>
-                    <h4>根据trans hash获取交易信息</h4>
+                    <h4>根据trans hash获取交易信息/get transaction info from trans hash</h4>
                     <textarea value={this.state.transHash} onChange={this.onGetTransHashChange}/>
                     <br/>
-                    <input type="button" value={"查询"} onClick={this.getTransFromHash}/>
+                    <input type="button" value={"查询/query"} onClick={this.getTransFromHash}/>
                     <br/><br/>
                     <table>
                         <tbody>
